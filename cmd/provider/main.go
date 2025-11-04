25085f23b36d8025ff11951d9145cec5f7de753c
Damien-DAGORN <damien.dagorn@evaneos.com>
2025-11-04 16:27:54 +0100

    fix: use the right name for logger

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -83,7 +83,7 @@ func main() {
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	zl := zap.New(zap.UseDevMode(*debug))
-	log := logging.NewLogrLogger(zl.WithName("upjet-provider-template"))
+	log := logging.NewLogrLogger(zl.WithName("provider-rabbitmq"))
 	if *debug {
 		// The controller-runtime runs with a no-op logger by default. It is
 		// *very* verbose even at info level, so we only provide it a real
